130f7d9dd015e06a65d49ef2b3e9cec6d36a69ee
nBow <nbow51@gmail.com>
2019-02-20 18:42:55 -0800

    Updates

---
+++ precache-manifest.6e8df584318bba2127ed4e21c7e58c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a95c6c5f4ffc677c465",
+    "url": "/BowArrow/click-it/static/js/runtime~main.7a95c6c5.js"
+  },
+  {
+    "revision": "c692a719d8f9ad8390d7",
+    "url": "/BowArrow/click-it/static/js/main.c692a719.chunk.js"
+  },
+  {
+    "revision": "65ada2a4a6a958fe707e",
+    "url": "/BowArrow/click-it/static/js/2.65ada2a4.chunk.js"
+  },
+  {
+    "revision": "c692a719d8f9ad8390d7",
+    "url": "/BowArrow/click-it/static/css/main.9e547a9d.chunk.css"
+  },
+  {
+    "revision": "2b711e893c960efe7b7fed3c954b52b7",
+    "url": "/BowArrow/click-it/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/BowArrow/click-it/precache-manifest.6e8df584318bba2127ed4e21c7e58c31.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/BowArrow/click-it/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
